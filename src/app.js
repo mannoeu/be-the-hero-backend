59db140e72c5cd471fee929fa2d2cd4d35883762
MANN03U <emmanuel_costasilva@hotmail.com>
2020-03-28 18:16:02 -0300

    correção nas permissões de acesso

--- src/app.js
+++ src/app.js
@@ -5,15 +5,14 @@ const routes = require("./routes");
 
 const app = express();
 
-app.use(cors());
-app.use(express.json());
-app.use(routes);
-app.use(errors());
-
 app.use(
   cors({
     origin: "https://bethehero-rocketseat.netlify.com/"
   })
 );
+
+app.use(express.json());
+app.use(routes);
+app.use(errors());
 
 module.exports = app;
